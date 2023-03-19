8ffff788051d43b00f089d04fd0b456e420a9566
Tony <tonyng147@gmail.com>
2023-03-19 13:40:22 +0700

    add comments

--- Signals/connection.h
+++ Signals/connection.h
@@ -12,6 +12,16 @@ class Signal{};
             bool _inConnect = true;
         }; 
         typedef std::shared_ptr<ConnectState> ConnectObject;
+/**
+ * This class is intended to implement the signal slot mechanism
+ * One signsl when invoked will emit all the connected slots
+ *
+ * ...
+ *
+ * @tparam _SIG The return type of function
+ * @tparam Args The argument list 
+ *              Argument list can accept as many parameters as possible
+ */
 template<typename _SIG,typename... Args>
 class Signal<_SIG(Args...)>
 {
@@ -19,13 +29,27 @@ class Signal<_SIG(Args...)>
         #define FUNC_PROT std::function<_SIG(Args...)>
         #define INS_REF  std::weak_ptr<ConnectState>
         #define LIST_FUNC_REF std::list<std::pair<INS_REF,FUNC_PROT>>
-        LIST_FUNC_REF m_listConnections;
-        std::shared_mutex    m_threadProtected;
+        LIST_FUNC_REF m_listConnections;                // The connected list 
+        std::shared_mutex    m_threadProtected;         // To protect shared environment 
+                                                        // in case of multithreading
     public:
         Signal() = default;
+        /*
+            @brief    This function is used to connect slot
+            @details  The connected slots (functions) will be called after
+                      the invocation of the registered function
+            @param    func The function
+        */
         ConnectObject connect(FUNC_PROT func);
+        /*
+            @brief   The call method of the function
+            @param   The variadic parameters
+        */
         void operator() (Args...);
 };
+/*
+
+*/
 template<typename _SIG,typename... Args>
 std::shared_ptr<ConnectState> Signal<_SIG(Args...)>::connect(FUNC_PROT func){
     std::unique_lock<std::shared_mutex> lock(m_threadProtected);
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
-#This project intents to demonstrate the mechanism signal slots of Qt
+# This project intents to demonstrate the mechanism signal slots of Qt
+
 **How**
 - To run this project, exec the script.sh
